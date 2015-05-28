be929d38d6a2ae3c9fa8809ced39453b67ecd052
Moritz Lawitschka <me@moritzlawitschka.de>
2015-05-28 16:16:02 +0200

    Fix: Use absolute path to binaries in build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,2 @@
-coffee --bare --compile --output lib/ src/
-browserify lib/galao.js -o lib/galao.browser.js
+./node_modules/.bin/coffee --bare --compile --output lib/ src/
+./node_modules/.bin/browserify lib/galao.js -o lib/galao.browser.js
